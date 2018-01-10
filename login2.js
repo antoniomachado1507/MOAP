154e9cc4c264fc825bce58615533807304d60f2a
Antonio Machado <antoniomachado1507@gmail.com>
2018-01-10 10:09:02 -0500

    Subida de prueba

---
+++ login2.js
@@ -0,0 +1,24 @@
+﻿var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'antoniomachado1507@gmail.com',
+    pass: 'atnightwhenthestarslighupmyroom'
+  }
+});
+
+var mailOptions = {
+  from: 'antoniomachado1507@gmail.com',
+  to: 'andriu.montalvoc@gmail.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!RATA'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
